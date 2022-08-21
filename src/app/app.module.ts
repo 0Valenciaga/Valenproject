720dc3f17f80ae6adb3a8d74dd204039554da9d9
0Valenciaga <valenhzgo@gmail.com>
2022-08-20 21:00:00 -0300

    app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialmediaComponent } from './components/socialmedia/socialmedia.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    SocialmediaComponent
   ],
   imports: [
     BrowserModule,
